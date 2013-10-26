50e6e6e496d8949a64dda32a354d0d308a916d37
Kman25 <kevyugi@gmail.com>
2013-10-26 11:47:03 +0100

    Added Sass files + added grid to the DOM

---
+++ scss/config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "../css"
+sass_dir = "./"
+images_dir = "../images"
+javascripts_dir = "../js"
+environment = :development
+relative_assets = true
+output_style = :expanded
+preferred_syntax = :scss
