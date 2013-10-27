bb96779775bdd2f77eca9a6ddb5ac4409111404b
Kman25 <kevyugi@gmail.com>
2013-10-27 21:36:39 +0000

    Implemented solution for being able to win

--- src/app.js
+++ src/app.js
@@ -27,9 +27,24 @@ module.controller('gridController', ['$scope', 'boardModel', 'playerModel', func
 	var board = $scope.board = boardModel;
 	var players = $scope.players = playerModel;
 	var currentTurn = 1;
+	var currentPlayer;
 
 	$scope.setPiece = function(index) {
-		board[index] = players[currentTurn%2].symbol;
+		currentPlayer = players[currentTurn%2]
+		board[index] = currentPlayer.symbol;
+		console.log(checkIfWinner())
 		currentTurn += 1;
+	}
+
+	var checkIfWinner = function() {
+		var winningRegex = new RegExp(currentPlayer.symbol + "{3}");
+		return (board[0] + board[1] + board[2]).search(winningRegex) != -1
+            || (board[3] + board[4] + board[5]).search(winningRegex) != -1
+            || (board[6] + board[7] + board[8]).search(winningRegex) != -1
+            || (board[0] + board[3] + board[6]).search(winningRegex) != -1
+            || (board[1] + board[4] + board[7]).search(winningRegex) != -1
+            || (board[2] + board[5] + board[8]).search(winningRegex) != -1
+            || (board[0] + board[4] + board[8]).search(winningRegex) != -1
+            || (board[2] + board[4] + board[6]).search(winningRegex) != -1;
 	}
 }]);
