70cb41f8934eb19709ee24fbcadee557249d79ac
Kman25 <kevyugi@gmail.com>
2013-10-28 01:51:05 +0000

    Added turn count

--- src/app.js
+++ src/app.js
@@ -21,25 +21,25 @@ module.factory('playerModel', function() {
 module.controller('gridController', ['$scope', 'playerModel', function($scope, playerModel) {
 	var board = $scope.board = ['', '', '', '', '', '', '' ,'' ,''];
 	var players = $scope.players = playerModel;
-	var currentTurn = 1;
 	var currentPlayer;
 	var gameOver = false;
 
+	$scope.currentTurn = 1;
+	
 	$scope.setPiece = function(index) {
-		if ((board[index] == '') && !gameOver) {
-			currentPlayer = players[currentTurn % 2]
-			board[index] = currentPlayer.symbol;
 		
-			if(_checkIfWinner()) {
-				gameOver = true;
-			}	
+		if ((board[index] == '') && !gameOver) {
+			currentPlayer = players[$scope.currentTurn % 2];
+			board[index] = currentPlayer.symbol;
+			gameOver = _checkIfWinner();
+			$scope.currentTurn += 1;
 		}
-
-		currentTurn += 1;
+		
 	}
 
 	var _checkIfWinner = function() {
 		var winningRegex = new RegExp(currentPlayer.symbol + "{3}");
+
 		return (board[0] + board[1] + board[2]).search(winningRegex) != -1
             || (board[3] + board[4] + board[5]).search(winningRegex) != -1
             || (board[6] + board[7] + board[8]).search(winningRegex) != -1
