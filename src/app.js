375e4f2354f1d40e07e480d244043df576d1a451
Kman25 <kevyugi@gmail.com>
2013-10-28 18:15:31 +0000

    More refactoring

--- src/app.js
+++ src/app.js
@@ -1,38 +1,48 @@
 var module = angular.module('app', []);
 
+module.value('readyToPlay', false);
+module.value('currentTurn', 1);
+
 module.factory('playerModel', function() {
 	var player = function(symbol) {
 		this.name = '';
 		this.symbol = symbol;
-
-		this.getSymbol = function() {
-			return this.symbol;
-		}
-
-		this.getName = function() {
-			return this.name;
-		}
 	}
 
 	return player;
 });
 
-module.service('playerCollection', function() {
-	return {
-		players : [],
-		add : function(player) {
-			this.players.push(player);
-		},
-		get : function(index) {
-			return this.players[index];
-		},
-		size : function() {
-			return this.players.length;
-		},
-		getAsArray : function() {
-			return this.players;
+module.service('playerCollection', 
+	function(currentTurn) {
+		return {
+			players : [],
+			add : function(player) {
+				this.players.push(player);
+			},
+			get : function(index) {
+				return this.players[index];
+			},
+			size : function() {
+				return this.players.length;
+			},
+			getAsArray : function() {
+				return this.players;
+			},
+			areNamesSet : function() {
+				return this._pluck('name').indexOf('') != -1
+			},
+			getCurrentPlayer : function (turn) {
+				return this.players[turn % 2];
+			},		
+			_pluck : function (field) {
+				var arr = [];
+				for (var i = 0; i < this.players.length; i ++) {
+					arr.push(this.players[i][field]);
+				}
+
+				return arr;
+			}
 		}
-	}
 });
 
 module.controller('playerController', ['$scope', 'playerModel', 'playerCollection', 'readyToPlay', 
@@ -48,7 +58,7 @@ module.controller('playerController', ['$scope', 'playerModel', 'playerCollectio
 		$scope.playerIndex = 0;
 
 		$scope.submitName = function() {
-			if( $scope.playerIndex < players.size() - 1) {
+			if(players.areNamesSet()) {
 				$scope.playerIndex += 1; 
 			} else {
 				$scope.readyToPlay = true;
@@ -57,62 +67,59 @@ module.controller('playerController', ['$scope', 'playerModel', 'playerCollectio
 	}
 ]);
 
-module.value('readyToPlay', false);
 
-
-module.directive('playerForm', function() {
-	return { 
-		restrict:'E',
-		controller:'playerController',
-		template: "<section ng-hide=\"readyToPlay\">\
-					<h1>Player {{playerIndex + 1}}</h1>\
-					<h1>Enter your name</h1>\
-					<input type=\"text\" ng-model=\"players.get(playerIndex).name\"/>\
-					<button ng-click=\"submitName()\">Submit</button>\
-					</section>"
-	}
+module.directive('playerForm', 
+	function() {
+		return { 
+			restrict:'E',
+			controller:'playerController',
+			template: "<section ng-hide=\"readyToPlay\">\
+						<h1>Player {{playerIndex + 1}}</h1>\
+						<h1>Enter your name</h1>\
+						<input type=\"text\" ng-model=\"players.get(playerIndex).name\"/>\
+						<button ng-click=\"submitName()\">Submit</button>\
+						</section>"
+		}
 	
 });
 
-
 //Change to boardController
-module.controller('gridController', ['$scope', 'playerCollection', function($scope, playerCollection) {
-	$scope.board = ['', '', '', '', '', '', '' ,'' ,''];
-	$scope.players = playerCollection;
-	$scope.currentPlayer;
-	$scope.gameOver = false;
-	$scope.currentTurn = 1;
+module.controller('boardController', ['$scope', 'playerCollection', 'currentTurn', 
+	function($scope, playerCollection, currentTurn) {
+		$scope.board = ['', '', '', '', '', '', '' ,'' ,''];
+		$scope.players = playerCollection;
+		$scope.gameOver = false;
+		$scope.currentTurn = currentTurn;
 
-	$scope.setPiece = function(index) {
-		
-		if (($scope.board[index] == '') && !$scope.gameOver) {
-			$scope.currentPlayer = $scope.players.get($scope.currentTurn % 2);
-			$scope.board[index] = $scope.currentPlayer.symbol;
-			
-			if(_checkIfWinner()) {
-				$scope.gameOver = true;
-			} else {
-				$scope.currentTurn += 1;
+		$scope.setPiece = function(index) {
+			if (($scope.board[index] == '') && !$scope.gameOver) {
+				$scope.board[index] = $scope.players.getCurrentPlayer($scope.currentTurn).symbol;
+				
+				if(_checkIfWinner()) {
+					$scope.gameOver = true;
+				} else {
+					$scope.currentTurn += 1;
+				}
 			}
 		}
-	}
 
-	$scope.reset = function() {
-		$scope.currentTurn = 1;
-		$scope.board = ['', '', '', '', '', '', '' ,'' ,''];
-		$scope.gameOver = false;
-	}
+		$scope.reset = function() {
+			$scope.currentTurn = 1;
+			$scope.board = ['', '', '', '', '', '', '' ,'' ,''];
+			$scope.gameOver = false;
+		}
 
-	var _checkIfWinner = function() {
-		var winningRegex = new RegExp($scope.currentPlayer.symbol + "{3}");
+		var _checkIfWinner = function() {
+			var winningRegex = new RegExp($scope.players.getCurrentPlayer($scope.currentTurn) + "{3}");
 
-		return ($scope.board[0] + $scope.board[1] + $scope.board[2]).search(winningRegex) != -1
-            || ($scope.board[3] + $scope.board[4] + $scope.board[5]).search(winningRegex) != -1
-            || ($scope.board[6] + $scope.board[7] + $scope.board[8]).search(winningRegex) != -1
-            || ($scope.board[0] + $scope.board[3] + $scope.board[6]).search(winningRegex) != -1
-            || ($scope.board[1] + $scope.board[4] + $scope.board[7]).search(winningRegex) != -1
-            || ($scope.board[2] + $scope.board[5] + $scope.board[8]).search(winningRegex) != -1
-            || ($scope.board[0] + $scope.board[4] + $scope.board[8]).search(winningRegex) != -1
-            || ($scope.board[2] + $scope.board[4] + $scope.board[6]).search(winningRegex) != -1;
+			return ($scope.board[0] + $scope.board[1] + $scope.board[2]).search(winningRegex) != -1
+	            || ($scope.board[3] + $scope.board[4] + $scope.board[5]).search(winningRegex) != -1
+	            || ($scope.board[6] + $scope.board[7] + $scope.board[8]).search(winningRegex) != -1
+	            || ($scope.board[0] + $scope.board[3] + $scope.board[6]).search(winningRegex) != -1
+	            || ($scope.board[1] + $scope.board[4] + $scope.board[7]).search(winningRegex) != -1
+	            || ($scope.board[2] + $scope.board[5] + $scope.board[8]).search(winningRegex) != -1
+	            || ($scope.board[0] + $scope.board[4] + $scope.board[8]).search(winningRegex) != -1
+	            || ($scope.board[2] + $scope.board[4] + $scope.board[6]).search(winningRegex) != -1;
+		}
 	}
-}]);
+]);
